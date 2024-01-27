2745ec049f10142e884c309498b990efae2c5bbc
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-01-26 20:12:58 -0500

    imp: Create todos from API

--- todobackend/api/urls.py
+++ todobackend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('todos/', views.TodoList.as_view()),
+    path('todos/', views.TodoListCreate.as_view()),
 ]
--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
-from .serializers import TodoSerializer
 from todo.models import Todo
+
+from .serializers import TodoSerializer
 
 # Create your views here.
 
 
-class TodoList(generics.ListAPIView):
+class TodoListCreate(generics.ListCreateAPIView):
     serializer_class = TodoSerializer
 
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
