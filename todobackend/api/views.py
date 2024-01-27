e8aa6284e02c682d34e818a3d0534de48c16fca4
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-01-27 10:13:53 -0500

    imp: Login and signup

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,7 +1,14 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.parsers import JSONParser
 from todo.models import Todo
 
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 
 # Create your views here.
 
@@ -25,3 +32,53 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user)
+
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not serializer.instance.completed
+        serializer.save()
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)  # data is a dictionary
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'username taken. choose another username'},
+                status=400)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(
+            request,
+            username=data['username'],
+            password=data['password'])
+        if user is None:
+            return JsonResponse(
+                {'error': 'could not login. please check username and/or password'},
+                status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=200)
